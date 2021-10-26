87860bea6a8127f44cbd71e732626921e330bd36
rbalykov <rbalykov@yandex.ru>
2021-10-26 11:02:04 +0300

    Delete build_armv7l directory

--- build_armv7l/DRM_Instance.d
+++
@@ -1,9 +0,0 @@
-build_armv7l/DRM_Instance.o: src/DRM_Instance.cpp \
- src/../include/DRM_Instance.h src/../include/common.h \
- /usr/include/libdrm/drm_fourcc.h
-
-src/../include/DRM_Instance.h:
-
-src/../include/common.h:
-
-/usr/include/libdrm/drm_fourcc.h:
